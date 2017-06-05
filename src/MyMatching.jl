27fb1302ab387218ae6af4e5feb1876749556134
taneaki <taneaki.m@gmail.com>
2017-06-06 00:12:37 +0900

    Update MyMatching.jl

--- src/MyMatching.jl
+++ src/MyMatching.jl
@@ -3,22 +3,22 @@ function my_deferred_acceptance(m_prefs, f_prefs)
     f_size = size(f_prefs, 2)
     m_matched = zeros(Int64, m_size)
     f_matched = zeros(Int64, f_size)
-    next_prop = zeros(Int64, m_size)#プロポーズする相手の選好順
+    next_prop = zeros(Int64, m_size)
     max_prop = Int64[]
     
     for m in 1:m_size
-        push!(max_prop, find(m_prefs[:, m] .== 0)[1]-1)#各男性の最大プロポーズ回数
+        push!(max_prop, find(m_prefs[:, m] .== 0)[1]-1)
     end
     
     while any(m_matched .== 0) == true
-        m_single = find(m_matched .== 0)#独身男性一覧
-        if all(next_prop[m_single] .>= max_prop[m_single]) == true#全ての男性の選好が0になるときbreak
+        m_single = find(m_matched .== 0)
+        if all(next_prop[m_single] .>= max_prop[m_single]) == true
             break
         else
-            for each_m_single in m_single#それぞれの独身男性について
-                proposing = m_prefs[next_prop[each_m_single]+1, each_m_single]#プロポーズする相手
+            for each_m_single in m_single
+                proposing = m_prefs[next_prop[each_m_single]+1, each_m_single]
                 if proposing != 0
-                    next_prop[each_m_single] = next_prop[each_m_single]+1#次回プロポーズする相手の選好順
+                    next_prop[each_m_single] = next_prop[each_m_single]+1
                     if f_matched[proposing] == 0 && (find(f_prefs[:, proposing] .==each_m_single) .<  find(f_prefs[:, proposing] .==0)) == [true]
                         m_matched[each_m_single] = proposing
                         f_matched[proposing] = each_m_single
