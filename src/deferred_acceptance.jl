067bfb70a06ae72287d36bcb85b3e36e4bc12a68
taneaki <taneaki.m@gmail.com>
2017-07-03 19:14:18 +0900

    .

--- src/deferred_acceptance.jl
+++ src/deferred_acceptance.jl
@@ -2,15 +2,15 @@
 function my_deferred_acceptance(prop_prefs::Array{Int},
                                 resp_prefs::Array{Int},
                                 caps::Vector{Int})
-    prop_size = size(prop_prefs, 2) #prop主体数
-    resp_size = size(resp_prefs, 2) #pref主体数
-    prop_matched = zeros(Int64, prop_size) #propマッチ相手配列の初期化
-    resp_matched = zeros(Int64, sum(caps)) #resfマッチ相手配列の初期化
-    next_prop = zeros(Int64, prop_size) #propが次にプロポーズをする時の回数
-    max_prop = Int64[] #最大プロポーズ回数配列の初期化
+    prop_size = size(prop_prefs, 2)
+    resp_size = size(resp_prefs, 2)
+    prop_matched = zeros(Int64, prop_size)
+    resp_matched = zeros(Int64, sum(caps))
+    next_prop = zeros(Int64, prop_size)
+    max_prop = Int64[]
     
     for p in 1:prop_size
-        push!(max_prop, find(prop_prefs[:, p] .== 0)[1]-1) #最大プロポーズ回数配列の設定
+        push!(max_prop, find(prop_prefs[:, p] .== 0)[1]-1)
     end
     
     indptr = Array{Int}(resp_size+1)
@@ -20,19 +20,19 @@ function my_deferred_acceptance(prop_prefs::Array{Int},
     end
     
     while any(prop_matched .== 0) == true
-        prop_single = find(prop_matched .== 0) #マッチしてない学生一覧
-        if all(next_prop[prop_single] .>= max_prop[prop_single]) == true #全員が最大プロポーズ回数をこえていればbreak
+        prop_single = find(prop_matched .== 0)
+        if all(next_prop[prop_single] .>= max_prop[prop_single]) == true
             break
         else
             for each_prop_single in prop_single
-                proposing = prop_prefs[next_prop[each_prop_single]+1, each_prop_single] #次にプロポーズする相手
+                proposing = prop_prefs[next_prop[each_prop_single]+1, each_prop_single]
                 if proposing != 0
                     next_prop[each_prop_single] = next_prop[each_prop_single]+1
                     if sum(resp_matched[indptr[proposing]:indptr[proposing+1]-1] .== 0) != 0 && (find(resp_prefs[:, proposing] .==each_prop_single) .<  find(resp_prefs[:, proposing] .==0)) == [true]
                         prop_matched[each_prop_single] = proposing
                         matched_index = findfirst(resp_matched[indptr[proposing]:indptr[proposing+1]-1] .== 0)
                         resp_matched[matched_index + indptr[proposing] - 1] = each_prop_single
-                    elseif sum(resp_matched[indptr[proposing]:indptr[proposing+1]-1] .== 0) .== 0 #respに0の枠あり
+                    elseif sum(resp_matched[indptr[proposing]:indptr[proposing+1]-1] .== 0) .== 0
                         current_order = Int64[]
                         for i in 1:caps[proposing] push!(current_order, find(resp_prefs[:, proposing] .== resp_matched[indptr[proposing]+i-1])[1]) end
                         if all(findmax(current_order)[1] .> find(resp_prefs[:, proposing] .== each_prop_single)) == true
